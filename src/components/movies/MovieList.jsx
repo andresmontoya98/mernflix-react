24d0e82e01c9b3b83abfee8b22581d6bc4ecc246
Andresrock98 <1dandresmontoya@gmail.com>
2022-03-23 19:26:02 +0100

    comentario1

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -3,7 +3,7 @@ import { Row, Col, Container, Badge, CardTitle } from 'reactstrap';
 import { getAllMovies } from "../../utils/apicalls.js";
 
 import Header from '../Header.jsx';
-import CardMovie from './CardMovie.jsx';
+//import CardMovie from './CardMovie.jsx';
 
 export default function MovieList(){
 
@@ -41,7 +41,7 @@ export default function MovieList(){
             {movies.map((movie) => {
                 return ( 
                   <Col xs ="3" sm="3" xs="4">
-                    
+                  <CardMovie movie={movie} />
                   </Col>
                 )
             })}
---
+++ src/utils/apicalls.js
@@ -0,0 +1,17 @@
+import API from './api';
+
+export{
+    getAllMovies,
+    addNewBookmark
+}
+
+function getAllMovies() {
+    return API.get('/movies').then(res => res.data);
+}
+
+
+function addNewBookmark(email, movie){
+    return API.post('/bookmarks', {
+        email,
+        movie}).then(result => result.data);
+}
