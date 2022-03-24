fde9d9a19c34bc871dc029a03f2a72c5ed4f30c1
Andresrock98 <1dandresmontoya@gmail.com>
2022-03-24 09:29:08 +0100

    comentario algo

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Login from "./Login";
+
+import Login from './Login';
 import MovieList from './movies/MovieList';
 import ShowMovie from './movies/ShowMovie';
 
 function App() {
   return (
     <Router>
-    <div>
-      <Routes>
-        <Route exact path="/" element={<Login/>} />
-        <Route path="/home" element={<MovieList/>} />
-        <Route path="/home/details/:id" element={<ShowMovie/>} />
-      </Routes>
-    </div>
-  </Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Login/>} />
+          <Route path="/home" element={<MovieList/>} />
+          <Route path="/home/details/:id" element={<ShowMovie/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ const config = {
     baseURL_API: process.env.REACT_APP_APIURI,
     clientID: process.env.REACT_APP_CLIENTID
  }
+  
 export default config;
